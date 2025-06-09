1cdf86b48320bd220657bbce43c36622ce927755
arkan <arkan.irsyad@gmail.com>
2025-06-09 15:57:54 +0700

    add initial commit

---
+++ Readme.md
@@ -0,0 +1,4 @@
+Brief Configuration to make it work
+-> Make Sure  run at least at node 18
+-> npm i
+-> npm start
---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import TrafficLight from './components/TrafficLight';
+
+export default function App() {
+  return <TrafficLight />;
+}
